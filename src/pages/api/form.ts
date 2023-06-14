b4b091055287cc18bbfed77f31eb64f806241bfc
colmpat <cplang@dons.usfca.edu>
2023-06-14 13:41:17 -0700

    fix: add promises to ensure email delivery in prod.

--- src/pages/api/form.ts
+++ src/pages/api/form.ts
@@ -1,6 +1,7 @@
-import { sendEmail } from './emails';
+import nodemailer from 'nodemailer';
+import { env } from "~/env.mjs";
 import { NextApiRequest, NextApiResponse } from "next";
-import { type interest as interestType  } from "~/components/Constants";
+import { type interest as interestType, emails, emailSubject, emailGreeting, interests } from "~/components/Constants";
 
 export type FormPayload = {
   name: string;
@@ -11,7 +12,7 @@ export type FormPayload = {
 };
 
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ){
@@ -20,10 +21,7 @@ export default function handler(
 
     // send email(s)
     for(let interest of payload.interests) {
-      sendEmail(payload, interest).catch(err => {
-        console.error(err);
-        res.status(500).end();
-      });
+      await sendEmail(payload, interest);
     }
      
     // update spreadsheet
@@ -34,3 +32,55 @@ export default function handler(
   }
 }
 
+
+/**
+  * Send an email to the given address for the given interest
+*/
+async function sendEmail({ name, email }: FormPayload, interest: interestType) {
+  console.log(`Sending email to ${email} for ${interests[interest]}`);
+  //
+  // Send the email
+  const mailData = {
+    from: 'Here2Stay <info@here2stay.com>',
+    to: email,
+    subject: emailSubject(interests[interest]),
+    text: emailGreeting(name) + emails[interest],
+  };
+
+  // create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: 'smtp.gmail.com',
+    auth: {
+      user: env.EMAIL_ADDR,
+      pass: env.EMAIL_PASS,
+    },
+    secure: true,
+  });
+
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+        if (error) {
+            console.log(error);
+            reject(error);
+        } else {
+            console.log("Server is ready to take our messages");
+            resolve(success);
+        }
+    });
+  });
+
+  await new Promise((resolve, reject) => {
+    // send mail
+    transporter.sendMail(mailData, (err, info) => {
+        if (err) {
+            console.error(err);
+            reject(err);
+        } else {
+            console.log(info);
+            resolve(info);
+        }
+    });
+  });
+}
